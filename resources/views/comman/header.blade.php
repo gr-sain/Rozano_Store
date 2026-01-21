98870cd7107c7a05e6a6e24741a6039ceb19be2a
vishu660 <vishaltak591@gamil.com>
2026-01-21 21:26:02 +0530

    facicon add

--- resources/views/comman/header.blade.php
+++ resources/views/comman/header.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('img/logo.svg') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <title>Ecommerce Website</title>
+    <title>Evara</title>
 </head>
 <body>
     <header class="header">
